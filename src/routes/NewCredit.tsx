7b37b4d46d380f8437d801f3a1dfb9ca7e4133d1
RyanWright <SivanoFFDS@gmail.com>
2022-02-12 19:01:40 +0700

    Add NewCredit

---
+++ src/routes/NewCredit.tsx
@@ -0,0 +1,62 @@
+import {
+  DatePicker,
+  DayOfWeek,
+  PrimaryButton,
+  Stack,
+  Text,
+  TextField,
+} from "@fluentui/react";
+import { FC, FormEventHandler, useState } from "react";
+import CardOnScreenCenter from "../molecules/CardOnScreenCenter";
+import storage from "../utils/storage";
+
+const NewCredit: FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const name = String(formData.get("name"));
+    const cost = parseFloat(String(formData.get("cost")) || "0");
+
+    storage.addCredit(name, cost, {
+      startDate: selectedDate,
+    });
+  };
+
+  const dateSelectionHandler = (date: Date | null | undefined) => {
+    if (date) setSelectedDate(date);
+  };
+
+  return (
+    <CardOnScreenCenter>
+      <form onSubmit={submitHandler}>
+        <Stack tokens={{ childrenGap: 10 }}>
+          <Text as="h1" variant="xxLarge">
+            Создайте кредит
+          </Text>
+          <TextField name="name" label="Название или назначение" required />
+          <TextField
+            name="cost"
+            required
+            type="number"
+            label="Сумма"
+            prefix="₽"
+          />
+          <DatePicker
+            onSelectDate={dateSelectionHandler}
+            label="Дата траты"
+            firstDayOfWeek={DayOfWeek.Sunday}
+            showMonthPickerAsOverlay
+          />
+          <Stack.Item align="end">
+            <PrimaryButton text="Создать" type="submit" />
+          </Stack.Item>
+        </Stack>
+      </form>
+    </CardOnScreenCenter>
+  );
+};
+
+export default NewCredit;

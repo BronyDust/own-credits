1ff05fe352c62ea2f8078d36b5bb613bb47a4331
RyanWright <SivanoFFDS@gmail.com>
2022-02-12 17:30:10 +0700

    Transform AppRouter to const function declaration

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 
-function AppRouter() {
-  return (
-    <Routes>
-      <Route path="/" element={<></>} />
-    </Routes>
-  );
-}
+const AppRouter = () => (
+  <Routes>
+    <Route path="/" element={<></>} />
+  </Routes>
+);
 
 export default AppRouter;

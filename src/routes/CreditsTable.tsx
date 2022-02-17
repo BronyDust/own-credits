463ac17da0036d6d98a1cb2eae6973daa2e2a0bb
RyanWright <SivanoFFDS@gmail.com>
2022-02-17 23:59:06 +0700

    Add title and button

--- src/routes/CreditsTable.tsx
+++ src/routes/CreditsTable.tsx
@@ -1,7 +1,22 @@
+import { DefaultButton, Stack, Text } from '@fluentui/react';
 import { FC } from 'react';
+import MainSurface from '../atoms/MainSurface';
 
 const CreditsTable: FC = () => {
-  return <div>sdfd</div>;
+  return (
+    <MainSurface>
+      <Stack>
+        <Text as="h1" variant="xxLarge">
+          Ваши кредиты
+        </Text>
+        <Stack.Item>
+          <DefaultButton iconProps={{ iconName: 'Add' }}>
+            Добавить
+          </DefaultButton>
+        </Stack.Item>
+      </Stack>
+    </MainSurface>
+  );
 };
 
 export default CreditsTable;

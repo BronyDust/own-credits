6b33ad661b0d2c49ba4879f0894a385ae9341392
RyanWright <SivanoFFDS@gmail.com>
2022-02-17 21:53:02 +0700

    Add debt logic to Credit class

--- src/utils/credit.class.ts
+++ src/utils/credit.class.ts
@@ -1,4 +1,5 @@
 import { v4 as uuid4 } from 'uuid';
+import Observable from './observable.abstract';
 
 type Payment = {
   cost: number;
@@ -6,18 +7,62 @@ type Payment = {
   description: string;
 };
 
-class Credit {
-  constructor(
-    private name: string,
-    private cost: number,
-    private additionalInfo: any = {},
-  ) {}
+export type PaymentsMap = Map<string, Payment>;
 
-  private payments = new Map<string, Payment>();
+class Credit extends Observable<PaymentsMap> {
+  private name: string;
+  private cost: number;
+  private additionalInfo: any;
+
+  constructor(name: string, cost: number, additionalInfo: any = {}) {
+    const payments: PaymentsMap = new Map();
+
+    super(payments);
+
+    this.payments = payments;
+    this.name = name;
+    this.cost = cost;
+    this.additionalInfo = additionalInfo;
+  }
+
+  private payments: PaymentsMap;
 
   addPayment(cost: number, date: string, description: string) {
     const uuid = uuid4();
     this.payments.set(uuid, { cost, date, description });
+
+    this.notifyListeners();
+  }
+
+  updatePayment(uuid: string, newData: Partial<Payment>) {
+    const prevState = this.payments.get(uuid);
+    if (!prevState) return;
+
+    this.payments.set(uuid, { ...prevState, ...newData });
+    this.notifyListeners();
+  }
+
+  deletePayment(uuid: string) {
+    this.payments.delete(uuid);
+    this.notifyListeners();
+  }
+
+  get debt() {
+    let debt = this.cost;
+
+    for (const payment of this.payments.values()) {
+      debt -= payment.cost;
+    }
+
+    return debt;
+  }
+
+  get metaData() {
+    return {
+      name: this.name,
+      cost: this.cost,
+      additionalInfo: this.additionalInfo,
+    };
   }
 }
 
